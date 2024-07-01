6d15ff37ba6ec342550fc4decf872786b656eec9
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-01 15:56:28 +0200

    images working
    media

--- stablexl/models.py
+++ stablexl/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Post(models.Model):  
     text = models.TextField()
+    img = models.ImageField(upload_to='image/', blank=True, null=True, default=None)
     def __str__(self):  
         return self.text[:50]
