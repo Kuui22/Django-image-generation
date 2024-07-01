2b1f7e31ce47ee1ed5d547ded02b347782055aea
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-01 10:27:17 +0200

    initial commit
    hello

---
+++ main.py
@@ -0,0 +1,23 @@
+#!pip install diffusers --upgrade
+#!pip install torch
+#!pip install accelerate
+#!pip install -U transformers
+#!pip install safetensors
+#!pip install -U peft
+
+#import torch
+#from diffusers import DiffusionPipeline
+#import peft
+
+#https://djangoforbeginners.com/hello-world/
+
+#venv = .venv
+#(.venv) $ python -m pip install django~=4.2.0
+#(.venv) $ python -m pip install black
+
+
+import gc
+
+def flush():
+    gc.collect()
+    #torch.cuda.empty_cache()
