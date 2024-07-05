fd6106d467243be3e59054930a6edb208ccef401
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-05 10:07:50 +0200

    updated tests

--- stablexl/tests.py
+++ stablexl/tests.py
@@ -37,3 +37,20 @@ class AboutpageTests(SimpleTestCase):
     def test_template_content(self):  # new
         response = self.client.get(reverse("about"))
         self.assertContains(response, "<h1>About page</h1>")
+
+
+class GeneratepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/generate/")
+        self.assertEqual(response.status_code, 200)
+    def test_url_available_by_name(self):  # new
+        response = self.client.get(reverse("generate"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("generate"))
+        self.assertTemplateUsed(response, "generate.html")
+
+    def test_template_content(self):  # new
+        response = self.client.get(reverse("generate"))
+        self.assertContains(response, "<h1>ciaossu</h1>")
