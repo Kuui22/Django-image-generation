877eaf4463fa529130708625f655ee8416578827
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-07-08 10:25:26 +0200

    GENERATION WORKING
    wooooooo

--- stablexl/tasks.py
+++ stablexl/tasks.py
@@ -5,15 +5,26 @@ import accelerate
 from diffusers import StableDiffusionPipeline
 import gc
 import os
+from PIL import Image
+import random
+import string
 logger = logging.getLogger(__name__)
 
 localmodel:str = 'static/stable1-5/'
 modelurl:str = "runwayml/stable-diffusion-v1-5" #https://huggingface.co/runwayml/stable-diffusion-v1-5
 
+#try to free up model from memory
 def flush():
     gc.collect()
     torch.cuda.empty_cache()
-
+    
+    
+#generates a string to append if the file name already exists
+def generate_random_string(length=6):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
+    
+@shared_task
 def testfolder():
     try:
         contents = os.listdir(localmodel)
@@ -26,12 +37,50 @@ def testfolder():
         logging.error(f"An error occurred: {e}")
 
 
-
-def rungeneration():
+@shared_task
+def rungeneration(prompt:str="A cat on a armchair high quality hd"):
+    logger.info("Prompt is:"+prompt)
     model_id:str = localmodel
-    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
-    pipe.enable_freeu(s1=0.9, s2=0.2, b1=1.5, b2=1.6) # https://huggingface.co/docs/diffusers/main/en/using-diffusers/image_quality
-    pipe.enable_model_cpu_offload()
+    try:
+        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+        #pipe.enable_freeu(s1=0.9, s2=0.2, b1=1.5, b2=1.6) # https://huggingface.co/docs/diffusers/main/en/using-diffusers/image_quality
+        pipe.enable_model_cpu_offload()
+        
+        image = pipe(
+            prompt,
+            negative_prompt="",
+            num_inference_steps=56,
+            guidance_scale=7.0,
+            height=512,
+            width=512,
+        ).images[0]
+        
+        directory = 'media/image'
+        base_filename:str = f"{prompt[:10].replace(' ', '_')}"
+        image_path = os.path.join(directory,f"{base_filename}.png").replace("\\", "/")
+        mediapath = os.path.join('image', f"{base_filename}.png").replace("\\", "/")
+        while os.path.exists(image_path):
+            random_suffix = generate_random_string()
+            image_path = os.path.join(directory, f"{base_filename}_{random_suffix}.png").replace("\\", "/")
+            mediapath = os.path.join('image', f"{base_filename}_{random_suffix}.png").replace("\\", "/")
+
+        image.save(image_path)
+        
+        
+        pipe.maybe_free_model_hooks()
+        del pipe
+        flush()
+        
+        return mediapath
+    except Exception as e:
+        logger.error(f"Error generating: {e}")
+        pipe.maybe_free_model_hooks()
+        del pipe
+        flush()
+        return None
+        
+    
+    
 
 
 
@@ -40,7 +89,7 @@ def add(x, y):
     return x + y
 
 @shared_task
-def generate(prompt):
+def generatelog(prompt):
     logger.info(prompt)
 
 @shared_task
--- stablexl/views.py
+++ stablexl/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseBadRequest,JsonResponse
 from django.views.generic import ListView,TemplateView
 from .models import Post
-from .tasks import log_message_task,generate,testfolder_task
+from .tasks import log_message_task,generatelog,testfolder_task,rungeneration
+from django.core.files import File
+import time
 # Create your views here.
 #def home_page_view(request):
 #    return HttpResponse("Hello, World!")
@@ -16,16 +18,37 @@ def trigger_generate(request):
         text = request.POST.get('text')
         if not text:
             return HttpResponseBadRequest("Text input cannot be empty")
-        generate.delay(text)
+        task = rungeneration.delay(text)
+        while not task.ready():
+            time.sleep(1)  # Sleep for a while before polling again
+        if task.successful():
+            image_path = task.result
+            if image_path:
+                createitem(image_path, text)
+        else:
+            generatelog("Task failed to generate image")
     return render(request, 'generate.html')
 
 def trigger_test_folder(request):
     testfolder_task.delay()  
     return render(request, 'generate.html')
 
+def createitem(image, txt):
+    try:
+        #Post.objects.create(img=img,text=txt)
+        post = Post(text=txt)
+        post.img.name = image
+        post.save()
+    except Exception as e:
+        generatelog(e)
+    
+    
+    
 class HomePageView(ListView):
     model = Post
     template_name = "home.html"
+
+
 
 class AboutPageView(TemplateView):  
     template_name = "about.html"    
